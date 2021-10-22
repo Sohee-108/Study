1d4183eee9b76495e223910a61e7690e233cfbc7
최소희 <choesohui@choesohuiui-MacBookAir.local>
2021-10-22 17:07:21 +0900

    2

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-alert("hi");
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("#login-form input");
+
+function onLoginSubmit(e){
+    e.preventDefault();
+    console.log(e);
+    const username = loginInput.value;
+    console.log(username);
+}
+
+loginForm.addEventListener("submit",onLoginSubmit);
