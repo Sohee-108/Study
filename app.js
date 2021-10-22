59c378eccf708710b40de047d00a53c419e8c7c8
최소희 <choesohui@choesohuiui-MacBookAir.local>
2021-10-22 17:07:43 +0900

    2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("#login-form input");
 
 function onLoginSubmit(e){
-    e.preventDefault();
+    e.preventDefault(); //브라우저의 기본동작을 막아줌(ex.새로고침)
     console.log(e);
     const username = loginInput.value;
     console.log(username);
